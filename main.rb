bdeff75d10c075b8726732a8e0dd7f41469e5edb
Saverio Miroddi <saverio.pub2@gmail.com>
2021-08-04 22:13:38 +0200

    Gosu: user-driven point

--- main.rb
+++ main.rb
@@ -2,24 +2,37 @@
 
 require 'gosu'
 
-TILE_WIDTH = 960
-TILE_HEIGHT = 600
+SCREEN_WIDTH = 960
+SCREEN_HEIGHT = 600
 
 class Test < Gosu::Window
+  include Gosu
+
+  attr_accessor :player_x, :player_y, :tiles
+
   def initialize
-    super(TILE_WIDTH, TILE_HEIGHT)
+    super(SCREEN_WIDTH, SCREEN_HEIGHT)
     self.caption = "Hello World!"
 
-    @tiles = Gosu::Image.load_tiles("assets/doge_space.jpg", TILE_WIDTH, TILE_HEIGHT)
+    self.player_x, self.player_y = SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2
+
+    self.tiles = Image.load_tiles("assets/doge_space.jpg", SCREEN_WIDTH, SCREEN_HEIGHT)
   end
 
   def update
-    # ...
+    close if button_down?(KB_ESCAPE)
+
+    self.player_y = (self.player_y - 1).clamp(0, SCREEN_HEIGHT) if button_down?(KB_W)
+    self.player_y = (self.player_y + 1).clamp(0, SCREEN_HEIGHT) if button_down?(KB_S)
+    self.player_x = (self.player_x - 1).clamp(0, SCREEN_WIDTH) if button_down?(KB_A)
+    self.player_x = (self.player_x + 1).clamp(0, SCREEN_WIDTH) if button_down?(KB_D)
   end
 
   def draw
-    tile_i = (Gosu.milliseconds / 1000) % @tiles.size
-    @tiles[tile_i].draw(0, 0, 0)
+    draw_rect(player_x, player_y, 1, 1, Color::YELLOW, 1)
+
+    tile_i = (milliseconds / 1000) % self.tiles.size
+    self.tiles[tile_i].draw(0, 0, 0)
   end
 end
 
